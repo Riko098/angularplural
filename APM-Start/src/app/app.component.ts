6025057d5ecca23b8020b5a2c0bbf51e22f29293
erico <ericopeixoto98@gmail.com>
2021-08-11 21:53:11 -0300

    enviado projeto para o git

---
+++ APM-Start/src/app/app.component.ts
@@ -0,0 +1,16 @@
+//importação do decorator @Component
+import { Component } from "@angular/core";
+//Decorator
+@Component({
+          //Metadados
+  //Diretiva
+  selector:'pm-root',
+  template:`
+  <div><h1>{{pageTitle}}</h1>
+  <pm-products></pm-products>
+</div>
+  `
+})
+export class AppComponent{
+  pageTitle:string="Acme Product Management";
+}

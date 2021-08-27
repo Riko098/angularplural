1155b1ed1155d17cc7e732d41a930fe0263020cc
erico <ericopeixoto98@gmail.com>
2021-08-27 08:53:22 -0300

    aplicando filtro no campo filtrar produto

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -12,7 +12,19 @@ export class ProductListComponent implements OnInit{
   imageWidth:number = 50;
   imageMargin:number =2;
   showImage:boolean = false;
-  listFilter:string = "cart";
+
+  private _listFilter:string = "";
+  get listFilter():string{
+    return this._listFilter
+  }
+  set listFilter(value:string){
+    this._listFilter=value;
+    console.log("In setter",value);
+    this.filteredProducts=this.performFilter(value);
+  
+  }
+  filteredProducts:IProduct[]=[];
+
   products:IProduct[]=[
     {
       "productId": 1,
@@ -43,6 +55,13 @@ export class ProductListComponent implements OnInit{
   }
 
   ngOnInit() {
-    console.log('In OnInit');
+    this.listFilter='cart'
+
   }
+  performFilter(filterBy:string):IProduct[]{
+    filterBy=filterBy.toLowerCase();
+    return this.products.filter((product:IProduct) =>
+    product.productName.toLocaleLowerCase().includes(filterBy))
+  }
+
 }

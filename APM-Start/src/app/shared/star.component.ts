7f894e1b4ba8f6843a8d6d1e6f2395e40d1d2849
erico <ericopeixoto98@gmail.com>
2021-08-29 16:27:18 -0300

    criação do component start-component

---
+++ APM-Start/src/app/shared/star.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component( {
+    selector:'pm-start',
+    templateUrl:'./star.component.html',
+    styleUrls:['./star.component.scss']
+  })
+export class StarComponent{
+
+}
